e857fb52487512cdc6bcdad864bc3f8c03f02794
Drew Lawless <126910584+ajl77@users.noreply.github.com>
2025-04-16 13:19:48 -0400

    Update heterogeneous_graph.py

--- heterogeneous_graph.py
+++ heterogeneous_graph.py
@@ -1,66 +1,92 @@
-import networkx as nx
 import os
 import json
+import networkx as nx
+from tqdm import tqdm
 
-def load_wallet_dev_edges(base_path, token_name):
-    dev_edges_path = os.path.join(base_path, "wallet_dev_edges", token_name)
-    dev_edges = []
-    for file in os.listdir(dev_edges_path):
-        with open(os.path.join(dev_edges_path, file), 'r') as f:
-            transaction = json.load(f)
-            dev_edges.append({
-                'wallet_address': transaction['from'],
-                'dev_address': transaction['to'],
-                'amount': transaction['amount']
-            })
-    return dev_edges
+def load_wallet_wallet_edges(path):
+    edges = []
+    for filename in os.listdir(path):
+        if filename.endswith(".json"):
+            with open(os.path.join(path, filename), "r") as f:
+                tx = json.load(f)
+                if tx["from"] and tx["to"]:
+                    edges.append((tx["from"], tx["to"], {"type": "wallet-wallet", "weight": tx["amount"]}))
+    return edges
 
+def load_wallet_dev_edges(path):
+    edges = []
+    for filename in os.listdir(path):
+        if filename.endswith(".json"):
+            with open(os.path.join(path, filename), "r") as f:
+                tx = json.load(f)
+                if tx["from"] and tx["to"]:
+                    edges.append((tx["from"], tx["to"], {"type": "wallet-dev", "weight": tx["amount"]}))
+    return edges
 
-def load_wallet_wallet_edges(base_path, token_name):
-    wallet_wallet_edges_path = os.path.join(base_path, "wallet_wallet_edges", token_name)
-    wallet_wallet_edges = []
-    for file in os.listdir(wallet_wallet_edges_path):
-        with open(os.path.join(wallet_wallet_edges_path, file), 'r') as f:
-            transaction = json.load(f)
-            wallet_wallet_edges.append({
-                'wallet_1': transaction['from'],
-                'wallet_2': transaction['to'],
-                'amount': transaction['amount']
-            })
-    return wallet_wallet_edges
-
-
-def load_wallet_token_edges(base_path, token_name):
-    wallet_token_edges_path = os.path.join(base_path, "wallet_token_edges", token_name)
-    wallet_token_edges = []
-    for file in os.listdir(wallet_token_edges_path):
-        with open(os.path.join(wallet_token_edges_path, file), 'r') as f:
-            transaction = json.load(f)
-            wallet_token_edges.append({
-                'wallet_address': transaction['wallet'],
-                'token_address': transaction['token'],
-                'amount': transaction['amount']
-            })
-    return wallet_token_edges
-
+def load_wallet_token_edges(path):
+    edges = []
+    for filename in os.listdir(path):
+        if filename.endswith(".json"):
+            with open(os.path.join(path, filename), "r") as f:
+                tx = json.load(f)
+                if tx["wallet_address"] and tx["token_address"]:
+                    edge_type = f"wallet-token-{tx['type']}"  
+                    edges.append((tx["wallet_address"], tx["token_address"], {
+                        "type": edge_type,
+                        "amount": tx["amount"],
+                        "priceUsd": tx["priceUsd"],
+                        "volume": tx["volume"],
+                        "volumeSol": tx["volumeSol"],
+                        "time": tx["time"]
+                    }))
+    return edges
 
 def create_heterogeneous_graph(base_path, token_name):
+    wallet_wallet_path = os.path.join(base_path, "wallet_wallet_edges", token_name)
+    wallet_dev_path = os.path.join(base_path, "wallet_dev_edges", token_name)
+    wallet_token_path = os.path.join(base_path, "wallet_token_edges", token_name)
+
     G = nx.DiGraph()
-  
-    wallet_dev_edges = load_wallet_dev_edges(base_path, token_name)
-    wallet_wallet_edges = load_wallet_wallet_edges(base_path, token_name)
-    wallet_token_edges = load_wallet_token_edges(base_path, token_name)
 
-    for edge in wallet_dev_edges:
-        G.add_edge(edge['wallet_address'], edge['dev_address'], type='wallet-dev', amount=edge['amount'])
-    
-    for edge in wallet_wallet_edges:
-        G.add_edge(edge['wallet_1'], edge['wallet_2'], type='wallet-wallet', amount=edge['amount'])
-    
-    for edge in wallet_token_edges:
-        G.add_edge(edge['wallet_address'], edge['token_address'], type='wallet-token', amount=edge['amount'])
+    print("Loading wallet-wallet edges...")
+    wallet_wallet_edges = load_wallet_wallet_edges(wallet_wallet_path)
+    G.add_edges_from(wallet_wallet_edges)
+    print(f"Added {len(wallet_wallet_edges)} wallet-wallet edges.")
 
-    nx.write_gpickle(G, os.path.join(base_path, f"{token_name}_heterogeneous_graph.gpickle"))
-    print(f"Graph with {len(G.nodes)} nodes and {len(G.edges)} edges created.")
-  
+    print("Loading wallet-dev edges...")
+    wallet_dev_edges = load_wallet_dev_edges(wallet_dev_path)
+    G.add_edges_from(wallet_dev_edges)
+    print(f"Added {len(wallet_dev_edges)} wallet-dev edges.")
+
+    print("Loading wallet-token edges...")
+    wallet_token_edges = load_wallet_token_edges(wallet_token_path)
+    G.add_edges_from(wallet_token_edges)
+    print(f"Added {len(wallet_token_edges)} wallet-token edges.")
+
+    for node in G.nodes:
+        if node.startswith("token_"):
+            G.nodes[node]["type"] = "token"
+        elif node.startswith("dev_"):
+            G.nodes[node]["type"] = "dev"
+        else:
+            G.nodes[node]["type"] = "wallet"
+
     return G
+
+def save_graph(G, base_path, token_name):
+    os.makedirs(base_path, exist_ok=True)
+    path = os.path.join(base_path, f"{token_name}_heterogeneous_graph.gpickle")
+    nx.write_gpickle(G, path)
+    print(f"Graph saved to {path}")
+
+def main():
+    base_path = "/Users/drew/Desktop/CS/CS485/data" 
+    token_name = "r_pwease"  
+
+    print(f"Creating heterogeneous graph for token: {token_name}")
+    G = create_heterogeneous_graph(base_path, token_name)
+    save_graph(G, base_path, token_name)
+    print(f"Graph has {G.number_of_nodes()} nodes and {G.number_of_edges()} edges.")
+
+if __name__ == "__main__":
+    main()
